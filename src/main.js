6a6d2d54d907deae8adb291d3bfe2fdd29abb036
Nadia Mousaoui <kactusworld.design@gmail.com>
2026-05-12 07:13:37 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+import gsap from 'gsap'
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import { Observer } from "gsap/Observer";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(DrawSVGPlugin,GSDevTools,Observer,ScrollTrigger,SplitText);
+
+
+
+
